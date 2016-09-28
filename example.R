82260d10d71f2e5282637a4f298e1b5e0f9f71e8
muschellij2 <muschellij2@gmail.com>
2016-09-28 17:07:34 -0400

    moving httr

---
+++ example.R
@@ -0,0 +1,13 @@
+lesson_info = list(Course = "advdatasci_swirl", Lesson = "httr_and_rvest_1", 
+                   Author = "John Muschelli", Type = "Standard", 
+                   Organization = "JHU Biostatistics and DSL",
+                   Version = "2.4.2.3")
+# How do you assign 5 to x
+x <- 5
+
+# What happens when j = 4
+j <- 4
+## This is a message
+
+### This is a multiple choice question - first choice is always right one
+c(1,2,3,4)
