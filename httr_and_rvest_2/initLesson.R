72bb96b0c593e42ed04443da7098d4455ede6082
dengdetian0603 <dengdetian0603@gmail.com>
2016-10-19 13:34:16 -0400

    Merge branch 'jtleek/master' into caret1
    # Conflicts:
    #	MANIFEST

--- httr_and_rvest_2/initLesson.R
+++ httr_and_rvest_2/initLesson.R
@@ -2,8 +2,4 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
-<<<<<<< HEAD:Text_Manipulation/initLesson.R
- swirl_options(swirl_logging = TRUE)
-=======
 swirl_options(swirl_logging = TRUE)
->>>>>>> jtleek/master:httr_and_rvest_1/initLesson.R
